591166bf8e658cdfc1004fd650420b5ee581eae2
Marina Stankevich <stankevichmarina8@gmail.com>
2025-12-07 21:26:50 +0300

    Configure GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com'],
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/OF' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/OF' : '',
 }
 
 module.exports = nextConfig
